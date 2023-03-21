a50dff7a5fc453c790680e6bb37daaf7b63267a5
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-21 17:01:05 +0900

    des

--- layout/__init__.py
+++ layout/__init__.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 from data import get_questions, get_terms
 
 def main():
@@ -12,7 +13,7 @@ def main():
     chapter = ts[(ts.part == data.part) & (ts.chapter == data.chapter)].iloc[0,2]
     st.markdown(f"{part} / {chapter}")
     st.markdown(f"> {data.title}")
-    
+    st.write(data.description)
 def prev_page():
     if st.session_state['page'] > 0:
         st.session_state['page'] -= 1
