14534288c2489c476d91b2c6a6977fdc58a36e6c
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-21 14:09:42 +0900

    grid

--- layout/__init__.py
+++ layout/__init__.py
@@ -3,6 +3,9 @@ from data import get_questions
 
 def main():
     qs = get_questions()
-    st.button('◀️')
-    st.button('▶️')
+    col = st.columns(5)
+    with col[2]:
+        col1, col2 = st.columns(2)
+        col1.button('◀️')
+        col2.button('▶️')
     st.write(qs)
