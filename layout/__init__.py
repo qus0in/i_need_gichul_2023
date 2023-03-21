5ba361d506435b71ebb5924e628ead3c3a7cbcce
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-21 17:27:43 +0900

    query

--- layout/__init__.py
+++ layout/__init__.py
@@ -3,24 +3,30 @@ import numpy as np
 from data import get_questions, get_terms
 
 def main():
-    qs = get_questions()
-    ts = get_terms()
     if 'page' not in st.session_state:
         st.session_state['page'] = 0
+    with st.container(): question()
+
+def question():
+    qs, ts = get_questions(), get_terms()
     page = st.session_state['page']
     data = qs.iloc[page]
-    part = ts[(ts.part == data.part) & (ts.chapter == 0)].iloc[0,2]
-    chapter = ts[(ts.part == data.part) & (ts.chapter == data.chapter)].iloc[0,2]
+    get_name = lambda x: ts.query(f'part == {data.part}').query(f'chapter == {x}').iloc[0,2]
+    part = get_name(0)
+    chapter = get_name(data.chapter)
     st.markdown(f"*{part} / {chapter}*")
     st.markdown(f"> {data.title}")
     if not np.isnan(data.description):
         st.markdown(data.description)
     st.text_input('정답 입력', key='answer')
     answer = st.session_state['answer']
-    if st.button('제출하기',
-        disabled=len(answer)==0,
-        use_container_width=True):
-        if answer in data.answer.split('|'):
+    btn = {
+        'label': '제출하기',
+        'disabled': len(answer)==0,
+        'use_container_width': True,
+    }
+    if st.button(**btn):
+        if answer.lower() in data.answer.split('|'):
             st.write('정답입니다')
         else:
             st.write('오답입니다')
