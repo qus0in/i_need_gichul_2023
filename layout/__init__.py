bc75ead55d043bdd8e89717a955578e6f901875b
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-21 14:12:25 +0900

    page

--- layout/__init__.py
+++ layout/__init__.py
@@ -4,8 +4,16 @@ from data import get_questions
 def main():
     qs = get_questions()
     col = st.columns(5)
+    st.session_state['page'] = 1
+
     with col[2]:
-        col1, col2 = st.columns(2)
-        col1.button('◀️')
-        col2.button('▶️')
+        col1, col2, col3 = st.columns(3)
+        col1.button('◀️', on_click=prev_page)
+        col2.write(st.session_state['page'])
+        col3.button('▶️', on_click=next_page)
     st.write(qs)
+
+def prev_page():
+    st.session_state['page'] -= 1
+def next_page():
+    st.session_state['page'] += 1
