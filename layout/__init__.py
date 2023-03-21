d5b289b6c60c4056ac9216261cd4097d3915ef6b
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-21 14:16:03 +0900

    paging

--- layout/__init__.py
+++ layout/__init__.py
@@ -1,21 +1,23 @@
 import streamlit as st
 from data import get_questions
 
+page = st.session_state['page']
+
 def main():
     qs = get_questions()
     col = st.columns(5)
     if 'page' not in st.session_state:
         st.session_state['page'] = 1
-    page = st.session_state['page']
-
+    
     with col[2]:
         col1, col2, col3 = st.columns(3)
         col1.button('◀️', on_click=prev_page)
-        col2.write(page)
+        col2.markdown(f"{page} / len(qs)")
         col3.button('▶️', on_click=next_page)
-    st.write(qs)
-
+    
 def prev_page():
-    st.session_state['page'] -= 1
+    if page > 1:
+        page -= 1
 def next_page():
-    st.session_state['page'] += 1
+    if page < len(get_questions()):
+        page += 1
