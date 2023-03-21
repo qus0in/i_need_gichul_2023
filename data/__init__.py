70cf8794631718532bccf95ba575f6e1681627fe
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-21 15:00:30 +0900

    merge

--- data/__init__.py
+++ data/__init__.py
@@ -5,3 +5,7 @@ import glob
 @st.cache_data
 def get_questions():
     return pd.concat([pd.read_csv(fn, sep=';') for fn in glob.glob('./data/question*.csv')])
+
+@st.cache_data
+def get_terms():
+    return pd.read_csv('terms.csv')
--- layout/__init__.py
+++ layout/__init__.py
@@ -1,24 +1,28 @@
 import streamlit as st
-from data import get_questions
+from data import get_questions, get_terms
 
 def main():
     qs = get_questions()
-    col = st.columns(5)
+    ts = get_terms()
+    df = qs.merge(ts)
     page = st.session_state['page']
-    
     if 'page' not in st.session_state:
-        st.session_state['page'] = 1
-    
+        st.session_state['page'] = 0
+
+    q = page.iloc[page]
+    st.write(q)
+
+    col = st.columns(5)
     with col[2]:
         col1, col2, col3 = st.columns(3)
         col1.button('◀️', on_click=prev_page)
-        col2.markdown(f"{page} / {len(qs)}")
+        col2.markdown(f"{page+1} / {len(qs)}")
         col3.button('▶️', on_click=next_page)
     
 def prev_page():
-    if st.session_state['page'] > 1:
+    if st.session_state['page'] > 0:
         st.session_state['page'] -= 1
 
 def next_page():
-    if st.session_state['page'] < len(get_questions()):
+    if st.session_state['page'] <= len(get_questions()):
         st.session_state['page'] += 1
